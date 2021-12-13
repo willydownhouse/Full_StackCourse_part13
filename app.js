3e0cfa4deae1a7ee11a9bf2525c879381115f15f
willydownhouse <ville.l.alatalo@gmail.com>
2021-12-13 15:27:18 +0100

    13.4

--- app.js
+++ app.js
@@ -1,9 +1,95 @@
+//const Blog = require("./models/BlogModel");
 const express = require("express");
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const app = express();
+app.use(express.json());
 
-app.get("/api/blogs", (req, res) => {
-  res.send("haloo lyngen");
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+const Blog = sequelize.define(
+  "Blog",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
+  }
+);
+
+app.get("/api/blogs", async (req, res) => {
+  try {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    console.log(req.body);
+    const blog = await Blog.create(req.body);
+
+    res.status(201).json(blog);
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(+req.params.id);
+
+    if (!blog) {
+      return res.status(404).send("No blog with that ID");
+    }
+    await Blog.destroy({
+      where: {
+        id: blog.dataValues.id,
+      },
+    });
+
+    res.status(200).end();
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
 });
 
 module.exports = app;
---
+++ models/BlogModel.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../app");
+
+const Blog = sequelize.define(
+  "Blog",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
+  }
+);
+
+Blog.sync();
+
+module.exports = Blog;
